10ec47ca7ce5ef823a88d431fed6754cef2df329
Dimid Ivanovich <vanakudinov140@gmail.com>
2026-05-08 22:20:33 +0500

    fix: remove Google Fonts dependency for build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,7 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import { Toaster } from "sonner";
 
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin", "cyrillic"] });
 
 export const metadata: Metadata = {
   title: "Регистрация посетителей музея",
@@ -14,7 +11,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="ru">
-      <body className={inter.className}>
+      <body className="font-sans">
         {children}
         <Toaster richColors position="top-right" />
       </body>
